72f427d2ecea4fa0ea5d2efe56a539e3a532c96b
Don Folk <donjfolk@gmail.com>
2021-10-23 13:54:14 -0600

    Removed tests info from df1

--- src/df1/df1.py
+++ src/df1/df1.py
@@ -235,21 +235,3 @@ class TcpMaster(object):
 			self._sock.setblocking(timeout_in_sec > 0)
 			if timeout_in_sec:
 				self._sock.settimeout(timeout_in_sec)
-
-
-
-
-oServer = TcpMaster(server="96.1.105.126", port=4103, src=0x0, dst=0x1, timeout_in_sec=5.0)
-aTag = [('I:1/0', 16),('B19:120/03', 1),('N7:0', 3),('F15:0', 2),('I:1/0', 16),('B19:120/03', 1),('N7:0', 3),('F15:0', 2),('I:1/0', 16),('B19:120/03', 1),('N7:0', 3),('F15:0', 2),('I:1/0', 16),('B19:120/03', 1),('N7:0', 3),('F15:0', 2),('I:1/0', 16),('B19:120/03', 1),('N7:0', 3),('F15:0', 2)]
-for tTag in aTag:
-	try:
-		print oServer.poll(tTag[0], tTag[1])
-	except Exception, ex:
-		print('Error:%s'%ex)
-
-
-
-#10 02 01 00 0F 00 CB 00 A2 02 01 8C 01 00 10 03 95 19 
-#10 02 01 00 0f 00 60 0c a2 01 01 8c 01 05 10 03 1d 18
-#16, 2, 0, 1, 79, 16, 16, 96, 12, 16, 3, 21, 192
-##aTag = ['N7:0','N7:1','N7:2','N7:3','','']
